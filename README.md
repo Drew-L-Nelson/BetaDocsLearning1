91ecbc69f942c6f02aeb4954465fc19ec8e9a14a
Drew-L-Nelson <90492724+Drew-L-Nelson@users.noreply.github.com>
2022-11-04 14:26:05 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BetaDocsLearning1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-4obhaw)
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.css";
+import { useState } from 'react';
+
+const PRODUCTS = [
+  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+];
+
+export default function App() {
+  return (
+    <div>
+      <h1>Components</h1>
+    </div>
+  );
+}
